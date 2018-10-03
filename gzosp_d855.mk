c583e8a18f9c841693ac814501482550b33b9220
doc HD <doc.divxm@gmail.com>
2018-10-03 10:47:42 +0300

    d855: GZOSPfy

--- gzosp_d855.mk
+++ gzosp_d855.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := gzosp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
